38ae91e799fbcd1fd1bb1c0c4216b9c3582840d5
Eduardo <eduardolustosa26@gmail.com>
2024-09-28 16:22:30 -0300

    feat; add cors middleware

--- internal/api/router/router.go
+++ internal/api/router/router.go
@@ -7,17 +7,28 @@ import (
 	"github.com/edulustosa/galleria/internal/api/middlewares"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func NewServer(pool *pgxpool.Pool, jwtKey string) http.Handler {
 	r := chi.NewMux()
 
+	corsMiddleware := cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	})
+
 	r.Use(
 		middleware.RequestID,
 		middleware.RealIP,
 		middleware.Logger,
 		middleware.Recoverer,
+		corsMiddleware,
 	)
 
 	addRoutes(r, pool, jwtKey)
